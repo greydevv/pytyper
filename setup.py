473f1f27c7151986acaaa2106d0a20ec6d445fd7
Greyson Murray <greyson.murray@gmail.com>
2020-10-24 21:28:20 -0400

    add MANIFEST file

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
-setup(name='pytyper',
-      version='0.1',
-      description='Typing statistics calculations, string comparison, and formatting',
-      url='https://github.com/greysonDEV/pytyper',
-      author='Greyson Murray',
-      author_email='greysonmurray.dev@gmail.com',
-      license='MIT',
-      packages=['pytyper'],
-      zip_safe=False)
+setup(
+	name='pytyper',
+	version='0.1',
+	description='Typing statistics calculations, string comparison, and formatting',
+	url='https://github.com/greysonDEV/pytyper',
+	author='Greyson Murray',
+	author_email='greysonmurray.dev@gmail.com',
+	license='MIT',
+	classifiers=[
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3.8",
+		"Topic :: Games/Entertainment",
+		"Operating System :: MacOS"
+	],
+	packages=['pytyper'],
+	zip_safe=False
+	)
